989336c3272f23bb4640db61858dbcbcd24e2c39
d_liyanage <admliyanage@gmail.com>
2020-09-23 20:25:14 +0530

    some changes done

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,134 @@
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <Nav class="mb-6" />
-    <HelloWorld msg="Welcome to Your Vue.js App" class="text-center"/>
+  <div
+    class="bg-gray-300 text-gray-800 antialiased min-h-screen flex flex-col justify-end"
+  >
+    <div class="bg-gray-400 p-8 flex justify-center">
+      <ul class="absolute -mt-32 space-y-2" style="bottom: 124px">
+        <li class="transform translate-x-10 rotate-12">
+          <a
+            href="#"
+            class="bg-white hover:bg-gray-100 w-80 shadow-md border rounded-lg p-4 flex items-center"
+          >
+            <img
+              src="https://s3.amazonaws.com/uifaces/faces/twitter/enda/128.jpg"
+              alt="avatar"
+              class="rounded-full w-10 h-10"
+            />
+            <div class="ml-4">
+              <div class="font-semibold">Mechanical Engineer</div>
+              <div class="text-left text-gray-700">Jason Freid</div>
+            </div>
+          </a>
+        </li>
+
+        <li class="transform translate-x-8 rotate-10">
+          <a
+            href="#"
+            class="bg-white hover:bg-gray-100 w-80 shadow-md border rounded-lg p-4 flex items-center"
+          >
+            <img
+              src="https://s3.amazonaws.com/uifaces/faces/twitter/woodydotmx/128.jpg"
+              alt="avatar"
+              class="rounded-full w-10 h-10"
+            />
+            <div class="ml-4">
+              <div class="font-semibold">Lecturer</div>
+              <div class="text-left text-gray-700">Michael Doe</div>
+            </div>
+          </a>
+        </li>
+
+        <li class="transform translate-x-6 rotate-8">
+          <a
+            href="#"
+            class="bg-white hover:bg-gray-100 w-80 shadow-md border rounded-lg p-4 flex items-center"
+          >
+            <img
+              src="https://s3.amazonaws.com/uifaces/faces/twitter/jeremyshimko/128.jpg"
+              alt="avatar"
+              class="rounded-full w-10 h-10"
+            />
+            <div class="ml-4">
+              <div class="font-semibold">Software Developer</div>
+              <div class="text-left text-gray-700">Jake Walker</div>
+            </div>
+          </a>
+        </li>
+
+        <li class="transform translate-x-4 rotate-6">
+          <a
+            href="#"
+            class="bg-white hover:bg-gray-100 w-80 shadow-md border rounded-lg p-4 flex items-center"
+          >
+            <img
+              src="https://s3.amazonaws.com/uifaces/faces/twitter/_scottburgess/128.jpg"
+              alt="avatar"
+              class="rounded-full w-10 h-10"
+            />
+            <div class="ml-4">
+              <div class="font-semibold">Graphic Designer</div>
+              <div class="text-left text-gray-700">Coleman Justin</div>
+            </div>
+          </a>
+        </li>
+
+        <li class="transform translate-x-2 rotate-4">
+          <a
+            href="#"
+            class="bg-white hover:bg-gray-100 w-80 shadow-md border rounded-lg p-4 flex items-center"
+          >
+            <img
+              src="https://s3.amazonaws.com/uifaces/faces/twitter/mattsapii/128.jpg"
+              alt="avatar"
+              class="rounded-full w-10 h-10"
+            />
+            <div class="ml-4">
+              <div class="font-semibold">Marketing Executive</div>
+              <div class="text-left text-gray-700">Zahir Abdal</div>
+            </div>
+          </a>
+        </li>
+
+        <li class="transform translate-x-1 rotate-2">
+          <a
+            href="#"
+            class="bg-white hover:bg-gray-100 w-80 shadow-md border rounded-lg p-4 flex items-center"
+          >
+            <img
+              src="https://s3.amazonaws.com/uifaces/faces/twitter/katiemdaly/128.jpg"
+              alt="avatar"
+              class="rounded-full w-10 h-10"
+            />
+            <div class="ml-4">
+              <div class="font-semibold">Project Manager</div>
+              <div class="text-left text-gray-700">Linda Grace</div>
+            </div>
+          </a>
+        </li>
+      </ul>
+
+      <button
+        class="bg-white hover:bg-gray-100 w-80 shadow-md border rounded-lg p-4 flex items-center -mt-16"
+      >
+        <img
+          src="https://s3.amazonaws.com/uifaces/faces/twitter/areandacom/128.jpg"
+          alt="avatar"
+          class="rounded-full w-10 h-10"
+        />
+        <div class="ml-4">
+          <div class="font-semibold">Hey Email Invite</div>
+          <div class="text-left text-gray-700">Newman Joseph</div>
+        </div>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Nav from './components/Nav.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Nav
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 
